4d696c0f17bf17791618f7d4dd631ff052f5df75
Alfred <alfredeveloper@gmail.com>
2020-02-22 13:01:14 -0500

    vuexy angular admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
+import { APP_ROUTES } from './app.routes';
+import { AdminComponent } from './admin/admin.component';
+import { AdminModule } from './admin/admin.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTES,
+    AdminModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
