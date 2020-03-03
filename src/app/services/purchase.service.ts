9304f4dee3cafb78cedfebbe4f1ff2970e272cb0
Alfred <alfredeveloper@gmail.com>
2020-03-03 16:10:13 -0500

    fix

--- src/app/services/purchase.service.ts
+++ src/app/services/purchase.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
-    'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU4MzIwNTcxNywiZXhwIjoxNTgzMjA5MzE3LCJuYmYiOjE1ODMyMDU3MTcsImp0aSI6IlBrdHJhWG56b3R0Zk54clAiLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.QrgA_mUrhuwOUKSLANQuZOgqfq_0GGZ6yg9w1KPSosc`
+    'Authorization': `Bearer ` + localStorage.getItem('token').toString()
   })
 };
 
